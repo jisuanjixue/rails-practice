3ce778d7e2f1f5d5cde9bf8b91925f978cfd7a4a
jisuanjixue <2078350663@qq.com>
2019-04-09 21:54:25 +0800

    用户可以自定义 ID

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,10 @@
 class Event < ApplicationRecord
 
- validates_presence_of :name
+ validates_presence_of :name, :random_id
+ validates_uniqueness_of :random_id, message: "必须不相同"
+ validates_format_of :random_id, with: /\A[a-z0-9\-]+\z/, message: "无效的格式"
 
- before_validation :generate_random_id, :on => :create
+ before_validation :generate_random_id, on: :create
 
  # 在调用路由方法时，Rails 默认都会用 to_param 方法来转换 ID,
 #  def to_param
