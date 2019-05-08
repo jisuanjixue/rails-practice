a760ae086ad6cf95b2e57b0c2ec0bb8b40e0e40e
jisuanjixue <2078350663@qq.com>
2019-05-08 12:00:29 +0800

    个人资料显示修改

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,30 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_user
+
+  def show
+  end
 
   def edit
-    @user = current_user
-      # 如果没有 @user.profile，要先新建一个
-    @user.create_profile unless @user.profile
   end
 
   def update
-    @user = current_user
-      if @user.update(user_params)
-        flash[:success] = "修改成功"
-        redirect_to edit_user_path
-      else
-        flash[:error] = "出错了"
-        render 'edit'
-      end
+    if @user.update(user_params)
+      flash[:success] = "修改成功"
+      redirect_to user_path
+    else
+      flash[:error] = "出错了"
+      render 'edit'
+    end
   end
   
   protected
+
+  def find_user
+    @user = current_user
+    # 如果没有 @user.profile，要先新建一个
+    @user.create_profile unless @user.profile
+  end
 
   def user_params
     params.require(:user).permit(:time_zone,  :profile_attributes => [:id, :legal_name, :birthday, :location, :education, :occupation, :bio, :specialty] )
