0d380989fcde61a868f99bf6e010dc851da1596e
jisuanjixue <2078350663@qq.com>
2019-05-09 14:34:23 +0800

    单独的resources编辑新增活动票种（CRUD）

---
+++ app/controllers/admin/event_tickets_controller.rb
@@ -0,0 +1,57 @@
+class Admin::EventTicketsController < AdminController
+  before_action :find_event
+  before_action :find_ticket, only: [:edit, :update]
+
+  def index
+    @tickets = @event.tickets
+  end
+
+  def new
+    @ticket = @event.tickets.new
+  end
+
+  def create
+    @ticket = @event.tickets.new(ticket_params)
+    if @ticket.save
+      redirect_to admin_event_tickets_path(@event)
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @ticket.update(ticket_params)
+      redirect_to admin_event_tickets_path(@event)
+    else 
+      render "edit"
+    end
+  end
+
+  def destroy
+    if @ticket.destroy
+      flash[:success] = '删除成功！'
+      redirect_to admin_event_tickets_path(@event)
+    else
+      flash[:error] = '删除失败'
+      redirect_to admin_event_tickets_path(@event)
+    end
+  end
+
+  protected
+
+  def find_event
+    @event = Event.find_by_random_id!(params[:event_id])
+  end
+
+  def find_ticket
+    @ticket = @event.tickets.find(params[:id])
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:name, :price, :description)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root "events#index"
-    resources :events
+    resources :events do
+      resources :tickets, :controller => "event_tickets"
+    end
     resources :users do
       # 因为默认的 controller 的命名是 profiles，这里我们偏好自定义命名改为user_profiles
       resource :profile, controller: "user_profiles" 
