34b502d060b0ddb0d4e6d2f1f243a6bc9a9b9dce
jisuanjixue <2078350663@qq.com>
2019-05-29 21:05:12 +0800

    时间区间筛选

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -20,6 +20,16 @@ class Admin::EventRegistrationsController < ApplicationController
     if Array(params[:ticket_ids]).any?
       @registrations = @registrations.by_ticket(params[:ticket_ids])
     end
+
+    # 时间段查询
+    if params[:start_on].present?
+      @registrations = @registrations.where( "created_at >= ?", Date.parse(params[:start_on]).beginning_of_day )
+    end
+      
+    if params[:end_on].present?
+      @registrations = @registrations.where( "created_at <= ?", Date.parse(params[:end_on]).end_of_day )
+    end
+      
   end
 
   def edit
