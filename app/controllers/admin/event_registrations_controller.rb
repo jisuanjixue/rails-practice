96d780e643fb0d1a181fbfab9dc03e900620f263
jisuanjixue <2078350663@qq.com>
2019-05-30 21:17:05 +0800

    汇出 CSV 档案

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Admin::EventRegistrationsController < ApplicationController
   before_action :find_event
   before_action :find_registration, only: %i[edit update destroy]
@@ -35,6 +37,20 @@ class Admin::EventRegistrationsController < ApplicationController
     # 字段查询，会要完全一模一样(exact match)才会筛选出来
     if params[:registration_id].present?
       @registrations = @registrations.where(id: params[:registration_id].split(','))
+    end
+
+    respond_to do |format|
+      format.html
+      format.csv do
+        @registrations = @registrations.reorder('id ASC')
+        csv_string = CSV.generate do |csv|
+          csv << ["报名ID", "票种", "姓名", "状态", "Email", "报名时间"]
+          @registrations.each do |r|
+            csv << [r.id, r.ticket.name, r.name, t(r.status, scope: 'registration.status'), r.email, r.created_at]
+          end
+        end
+        send_data csv_string, filename: "#{@event.random_id}-registrations-#{Time.now.to_s(:number)}.csv"
+      end
     end
   end
 
