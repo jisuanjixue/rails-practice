abbf18abfb9e8dc7d23ee4d48ac5d812014774dc
jisuanjixue <2078350663@qq.com>
2019-05-30 20:42:45 +0800

    分析票种的报名人数，并根据状态分类

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,11 +1,33 @@
 class Admin::EventsController < AdminController
-
   def index
     @events = Event.rank(:row_order).all
   end
 
   def show
     @event = Event.find_by_random_id!(params[:id])
+    colors = ['rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)']
+
+    ticket_names = @event.tickets.map(&:name)
+
+    status_colors = { "confirmed" => "#FF6384",
+      "pending" => "#36A2EB"}
+
+   @data1 = {
+       labels: ticket_names,
+       datasets: Registration::STATUS.map do |s|
+         {
+           label: I18n.t(s, :scope => "registration.status"),
+           data: @event.tickets.map{ |t| t.registrations.by_status(s).count },
+           backgroundColor: status_colors[s],
+           borderWidth: 1
+         }
+       end
+   }
   end
 
   def new
@@ -19,7 +41,7 @@ class Admin::EventsController < AdminController
     if @event.save
       redirect_to admin_events_path
     else
-      render "new"
+      render 'new'
     end
   end
 
@@ -34,7 +56,7 @@ class Admin::EventsController < AdminController
     if @event.update(event_params)
       redirect_to admin_events_path
     else
-      render "edit"
+      render 'edit'
     end
   end
 
@@ -51,15 +73,13 @@ class Admin::EventsController < AdminController
     total = 0
     Array(params[:ids]).each do |event_id|
       event = Event.find(event_id)
-        if params[:commit] == I18n.t(:bulk_update)
-          event.status = params[:event_status]
-          if event.save
-            total += 1
-          end
-        elsif params[:commit] == I18n.t(:bulk_delete)
-          event.destroy
-          total += 1
-        end
+      if params[:commit] == I18n.t(:bulk_update)
+        event.status = params[:event_status]
+        total += 1 if event.save
+      elsif params[:commit] == I18n.t(:bulk_delete)
+        event.destroy
+        total += 1
+      end
     end
 
     flash[:alert] = "成功完成 #{total} 项"
@@ -73,14 +93,13 @@ class Admin::EventsController < AdminController
 
     respond_to do |format|
       format.html { redirect_to admin_events_path }
-      format.json { render :json => { :message => "ok" }}
+      format.json { render json: { message: 'ok' } }
     end
   end
 
   protected
 
   def event_params
-    params.require(:event).permit(:name, :description, :random_id, :status, :remove_images, :category_id, :tickets_attributes => [:id, :name, :description, :price, :_destroy], :images => [])
+    params.require(:event).permit(:name, :description, :random_id, :status, :remove_images, :category_id, tickets_attributes: %i[id name description price _destroy], images: [])
   end
-
 end
